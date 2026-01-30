69bb5ff2c2cef36872d60467d510bec8eb3bea92
Manikandan A <mani26012004@gmail.com>
2026-01-30 04:24:12 -0330

    Add files via upload

---
+++ Course Registration System-Backend/Course-Registration-System/src/main/java/com/example/Course/Registration/System/controller/CourseController.java
@@ -0,0 +1,39 @@
+package com.example.Course.Registration.System.controller;
+
+import com.example.Course.Registration.System.model.Course;
+import com.example.Course.Registration.System.model.CourseRegistry;
+import com.example.Course.Registration.System.service.CourseService;
+import org.hibernate.boot.jaxb.Origin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+public class CourseController {
+
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping("courses")
+    public List<Course> avaliableCourse() {
+        return courseService.avaliableCourses();
+    }
+
+    @GetMapping("courses/endrolled")
+    public List<CourseRegistry> endRolledStudents(){
+        return  courseService.endrolledStudents();
+    }
+
+
+
+    @PostMapping("/courses/registered")
+    public String enrollCourse(@RequestParam("name") String name,
+                               @RequestParam("emailId") String emailId,
+                               @RequestParam("courseName") String courseName){
+        courseService.enrollCourse(name,emailId,courseName);
+        return "Congratulation! "+name+" Enrollment SuccessFull for "+courseName;
+    }
+
+}
